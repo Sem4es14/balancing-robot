c87005e28f1a1435457f662561df3b535703db1f
Semyon <ily-senin@yandex.ru>
2023-02-03 01:38:17 +0300

    project

---
+++ Core/Src/constants.h
@@ -0,0 +1,7 @@
+#define MPU6050_Address     0x68 << 1
+#define RAD_TO_DEG          180/3.14159265
+#define ACCEL_XOUT_H_REG    0x3B
+#define MAX_ANGLE           10000
+#define ANG_DIFF_FILT       0.01f
+#define SPEED_FILT          0.03f
+#define DELTA_TIME          0.002f
---
+++ Debug/Core/Src/MadgwickAHRS.d
@@ -0,0 +1,3 @@
+Core/Src/MadgwickAHRS.o: ../Core/Src/MadgwickAHRS.c \
+ ../Core/Src/MadgwickAHRS.h
+../Core/Src/MadgwickAHRS.h:
